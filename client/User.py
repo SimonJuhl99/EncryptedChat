a2b2c65315576db37da852c8cf936d5bab5faedd
Chejsing <46716091+Chejsing@users.noreply.github.com>
2022-11-22 19:52:22 +0100

    Added RSA-keys to User class

--- client/User.py
+++ client/User.py
@@ -1,10 +1,18 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
 
 class User:
 
     def __init__(self, user_id, alias, private_key):
         self.user_id = user_id
         self.alias = alias
-        self.private_key = private_key
+        self.private_key = rsa.generate_private_key(
+            public_exponent = 65537,
+            key_size = 2048,
+            backend = default_backend()
+        )
+
+        self.public_key = private_key.public_key()
 
     def change_password(self, old_password, new_password):
         pass
