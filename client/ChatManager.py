495e9a40c6b3a3c302fee5d375215bef6d065183
Simon Kaalund Juhl <sjuhl20@student.aau.dk>
2022-11-22 14:48:01 +0100

    working examples where serverside ChatManager echoes back to client

--- client/ChatManager.py
+++ client/ChatManager.py
@@ -1,16 +1,22 @@
 import socket
+
 ClientMultiSocket = socket.socket()
-host = '127.0.0.1'
+#host = '127.0.0.1'
+host = '192.168.1.105'
 port = 2004
 print('Waiting for connection response')
+
 try:
     ClientMultiSocket.connect((host, port))
 except socket.error as e:
     print(str(e))
+
 res = ClientMultiSocket.recv(1024)
+
 while True:
     Input = input('Hey there: ')
     ClientMultiSocket.send(str.encode(Input))
     res = ClientMultiSocket.recv(1024)
     print(res.decode('utf-8'))
+
 ClientMultiSocket.close()
--- server/ChatManager.py
+++ server/ChatManager.py
@@ -1,16 +1,24 @@
 import socket
 import os
 from _thread import *
+
 ServerSideSocket = socket.socket()
-host = '127.0.0.1'
+ServerSideSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+
+#host = '127.0.0.1'
+host = '192.168.1.105'
 port = 2004
 ThreadCount = 0
+
 try:
     ServerSideSocket.bind((host, port))
 except socket.error as e:
     print(str(e))
+
 print('Socket is listening..')
 ServerSideSocket.listen(5)
+
 def multi_threaded_client(connection):
     connection.send(str.encode('Server is working:'))
     while True:
@@ -20,10 +28,12 @@ def multi_threaded_client(connection):
             break
         connection.sendall(str.encode(response))
     connection.close()
+
 while True:
     Client, address = ServerSideSocket.accept()
     print('Connected to: ' + address[0] + ':' + str(address[1]))
     start_new_thread(multi_threaded_client, (Client, ))
     ThreadCount += 1
     print('Thread Number: ' + str(ThreadCount))
+
 ServerSideSocket.close()
