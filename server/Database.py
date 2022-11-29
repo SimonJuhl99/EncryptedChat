bf445184509eab41c04b5361a4537d4ef78d897a
AAUCrisp <stpe19@student.aau.dk>
2022-11-29 14:01:11 +0100

    Database general Insert function done, with message specific method included :unicorn:

--- server/Database.py
+++ server/Database.py
@@ -7,10 +7,10 @@ class Database():
     def __init__(self):
         # con = sqlite3.connect("database.db")    # Create connection to DB file
         # con = sqlite3.connect("database.db", detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)    # Create connection to DB file
-        con = sqlite3.connect("database.db", detect_types=sqlite3.PARSE_COLNAMES | sqlite3.PARSE_DECLTYPES)    # Create connection to DB file
+        self.con = sqlite3.connect("database.db", detect_types=sqlite3.PARSE_COLNAMES | sqlite3.PARSE_DECLTYPES)    # Create connection to DB file
         # con.row_factory = sqlite3.Row
-        con.row_factory = self.dict_factory
-        self.db = con.cursor()                        # Create a cursor in the DB
+        self.con.row_factory = self.dict_factory
+        self.db = self.con.cursor()                        # Create a cursor in the DB
         # self.db.mode("line")
         # subprocess.call(["sqlite3", "database.db", ".mode line"])
 
@@ -23,7 +23,13 @@ class Database():
         return d
 
 
-    # def fetch(self, table, params=None, join=None):
+##########################################
+##  --  Fetch Area --
+############
+
+
+    ######################################
+    #  --  General fetch function  --
     def fetch(self, table, setup = None):
         # print(table)
         # print(setup)
@@ -66,7 +72,7 @@ class Database():
                 sql += f"""{key}={config['where'][key]}"""
 
 
-        print(f"SQL Statement is:\n{sql}\n\n")
+        print(f"\nSQL Statement is:{sql}\n\n")
         self.db.execute(sql)
         rows = self.db.fetchall()
         # rows = self.db.fetchone()
@@ -85,7 +91,7 @@ class Database():
         print("Group fetching")
         db_config = {'where': {'group_member.group_id': group_id},
             'select': 'group_member.admin, user.alias',
-            'join':{'user': 'group_member.user_id=user.id'},
+            'join':{'user': 'group_member.user_id=user.id'}
             }
         
         self.fetch('group_member', db_config)
@@ -98,16 +104,64 @@ class Database():
 
 
 
+##########################################
+##  --  Insert Area --
+############
+
+
+    ######################################
+    #  --  General fetch function  --
+    def insert(self, table, params):
+
+        sql = f"""
+            INSERT INTO {table}
+            ("""
+
+        sql_end = ""
+
+
+        for i, key in enumerate(params):
+
+            if i>0:     # If not first column
+                sql += ", "
+                sql_end += ", "
+            
+            sql += f"{key}"
+
+            sql_end += f"'{params[key]}'"
+
+        sql += f""")
+            VALUES ({sql_end})"""
+
+
+        print(f"\nSQL Statement is:{sql}\n\n")
+        self.db.execute(sql)
+        self.con.commit()
+
+
+        print(f"Last Inserted Row is: {self.db.lastrowid}")
+
+        return self.db.lastrowid
+
+
+    # Method to log messages to database
+    def insert_msg(self, text, user_id, group_id):
+        params = {'text': text, 'user_id': user_id, 'group_id': group_id}
+        db.insert('message', params)
+
+
+
 if __name__ == "__main__":
     db = Database()
-    db_config = {'where': {'group_id': 1}}
-    # db.fetch("group_member", db_config)
 
-    # db_config = {'user_id': 1, 'group_id': 1}
-    # db.fetch("group_member", db_config)
-
+    # Fetch Testing
     db.fetch('user')
 
     db.fetch_group_members(1)
     db.fetch_own_key(3)
 
+
+    # Insert Testing
+    db.insert('message', {'text': 'ting er grimme', 'user_id': 2, 'group_id': 1})
+
+    db.insert_msg('næste besked', 1, 2)
